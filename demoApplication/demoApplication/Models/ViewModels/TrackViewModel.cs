ebe169629782a5198cd5e1ac8cddead3c86ee656
Yanny <mlb0815@yahoo.com.tw>
2019-12-09 16:19:45 +0800

    [HW1] 新增ViewModel

---
+++ demoApplication/demoApplication/Models/ViewModels/TrackViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkillTreeHomework.Models.ViewModels
+{
+    public class TrackViewModel
+    {
+        /// <summary>
+        /// 類別
+        /// </summary>
+        public CategoryType Category { get; set; }
+        /// <summary>
+        /// 金額
+        /// </summary>
+        public decimal Amount { get; set; }
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// 備註
+        /// </summary>
+        public string Note { get; set; }
+    }
+
+    public enum CategoryType
+    {
+        支出 = 0,
+
+        收入 = 1
+    }
+}
