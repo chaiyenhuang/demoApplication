7a33a7ae048a86b4cff1f21da96681d1a03e0b6e
Yanny <mlb0815@yahoo.com.tw>
2019-12-09 16:21:35 +0800

    [HW1]新增service層，寫入撈取資料邏輯

---
+++ demoApplication/demoApplication/Service/BookKeepingService.cs
@@ -0,0 +1,35 @@
+﻿using SkillTreeHomework.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkillTreeHomework.Service
+{
+    public class BookKeepingService
+    {
+        public IEnumerable<TrackViewModel> GetTrackdata()
+        {
+            var tracklist = new List<TrackViewModel>();
+            int num = 0;
+            //塞假資料100筆
+            for (int i = 0; i < 100; i++)
+            {
+                //隨機數字
+                num = new Random(Guid.NewGuid().GetHashCode()).Next(1,9999) + i;
+                TrackViewModel result = new TrackViewModel
+                {
+                    Category = (CategoryType)(num % 2),
+                    Amount = num,
+                    Date = DateTime.Now.AddDays(i)
+                };
+
+                //資料丟進List
+                tracklist.Add(result);
+            }
+
+
+            return tracklist;
+        }
+    }
+}
